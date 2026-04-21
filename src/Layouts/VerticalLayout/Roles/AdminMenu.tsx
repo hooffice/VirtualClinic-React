44bb18fa5d49dae2367f94aed9bab7f6f2bc2b61
AnjanPandya <access2apandya@gmail.com>
2026-04-21 01:46:38 -0400

    admin menu updated

--- src/Layouts/VerticalLayout/Roles/AdminMenu.tsx
+++ src/Layouts/VerticalLayout/Roles/AdminMenu.tsx
@@ -13,49 +13,97 @@ const AdminMenu = (props: any) => {
       </li>
 
       <li className="menu-title">{props.t("Management")}</li>
-
       <li>
         <Link to="#" className="has-arrow">
-          <i className="bx bx-user"></i>
-          <span>{props.t("Users")}</span>
+          <i className="mdi mdi-adjust"></i>
+          <span>{props.t("Setup")}</span>
         </Link>
         <ul className="sub-menu">
           <li>
-            <Link to="#">{props.t("All Users")}</Link>
+            <Link to="#">{props.t("Organization")}</Link>
           </li>
           <li>
-            <Link to="#">{props.t("Add User")}</Link>
+            <Link to="#">{props.t("Employer")}</Link>
           </li>
           <li>
-            <Link to="#">{props.t("User Roles")}</Link>
+            <Link to="#">{props.t("Clinic")}</Link>
           </li>
+          <li>
+            <Link to="#">{props.t("Clinician")}</Link>
+          </li> 
+          <li>
+            <Link to="#">{props.t("Patient")}</Link>
+          </li>
+          <li>
+            <Link to="#">{props.t("Lab Test")}</Link>
+          </li>
+          <li>
+            <Link to="#">{props.t("Medical Minute")}</Link>
+          </li>   
+          <li>
+            <Link to="#">{props.t("Labcorp PSC Search")}</Link>
+          </li>   
+          <li>
+            <Link to="#">{props.t("Stripe Download")}</Link>
+          </li> 
+          <li>
+            <Link to="#">{props.t("Stripe to QuickBook Sync")}</Link>
+          </li>                                             
         </ul>
+                     
       </li>
-
+      <li className="menu-title">{props.t("Analytics")}</li>
       <li>
         <Link to="#" className="has-arrow">
-          <i className="bx bx-cog"></i>
-          <span>{props.t("Settings")}</span>
+          <i className="bx bx-file"></i>
+          <span>{props.t("Reports")}</span>
         </Link>
         <ul className="sub-menu">
           <li>
-            <Link to="#">{props.t("General Settings")}</Link>
+            <Link to="#">{props.t("Lab Invoice")}</Link>
           </li>
           <li>
-            <Link to="#">{props.t("Security")}</Link>
+            <Link to="#">{props.t("Lab Order History")}</Link>
           </li>
           <li>
-            <Link to="#">{props.t("Permissions")}</Link>
+            <Link to="#">{props.t("OHS Order History")}</Link>
           </li>
+          <li>
+            <Link to="#">{props.t("Order Summary")}</Link>
+          </li> 
+          <li>
+            <Link to="#">{props.t("Refund")}</Link>
+          </li>
+          <li>
+            <Link to="#">{props.t("Lab Invoice Verification")}</Link>
+          </li>
+          <li>
+            <Link to="#">{props.t("Lab Results")}</Link>
+          </li>   
+          <li>
+            <Link to="#">{props.t("Unprocessed Lab")}</Link>
+          </li>   
+          <li>
+            <Link to="#">{props.t("Lab Review")}</Link>
+          </li> 
+          <li>
+            <Link to="#">{props.t("Favourite Labs By Clinician")}</Link>
+          </li>  
+          <li>
+            <Link to="#">{props.t("Sales Efforts")}</Link>
+          </li>    
+          <li>
+            <Link to="#">{props.t("Sales Analysis")}</Link>
+          </li>                                                              
         </ul>
-      </li>
-
-      <li>
+                     
+      </li>      
+      {/* <li>
         <Link to="#">
           <i className="bx bx-file"></i>
           <span>{props.t("Reports")}</span>
         </Link>
-      </li>
+      </li> */}
     </>
   );
 };
